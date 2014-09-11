7e84f6f1f94270f00574f5ec432122f290f1084d
Matt Bierbaum <matt.bierbaum@gmail.com>
2014-09-11 17:59:30 -0400

    broke of xpm for saving in node

---
+++ js/savexpm.js
@@ -0,0 +1,65 @@
+(function(exports){
+"use strict";
+
+/* a sample xmp file version 3 (i think) */
+/*
+    \* XPM *\
+    static char *purple_xpm[] = {
+        \* width height ncolors chars_per_pixel *\
+        "1 1 1 1",
+        \* colors *\
+        "a c purple",
+        \* pixels *\
+        "a"
+    };
+*/
+
+function xpm(sites){
+    var mins = {'x': 1e10, 'y': 1e10};
+    var maxs = {'x': -1e10, 'y': -1e10};
+
+    for (var c in sites){
+        var site = sites[c];
+        if (mins.x > site.x) { mins.x = site.x; }
+        if (mins.y > site.y) { mins.y = site.y; }
+        if (maxs.x < site.x) { maxs.x = site.x; }
+        if (maxs.y < site.y) { maxs.y = site.y; }
+    }
+
+    var height = maxs.y - mins.y + 1;
+    var width = maxs.x - mins.x + 1;
+
+    var out = "/* XPM */\n"
+            + "static char *mypic[] = {"
+            + '"'+width+" "+height+' 3 1"\n'
+            + '". c black"\n'
+            + '"X c red"\n'
+            + '"  c None"\n';
+
+    var arr = new Array(width*height);
+    for (var i=0; i<width*height; i++)
+        arr[i] = ' ';
+
+    for (var i in sites){
+        var site = sites[i];
+        var x = site.x - mins.x;
+        var y = site.y - mins.y;
+        var ind = x + y*width;
+
+        if (site.Z > 0) arr[ind] = 'X';
+        if (site.R > 0) arr[ind] = '.';
+    }
+
+    for (var i=0; i<height; i++){
+        out += '"';
+        for (var j=0; j<width; j++){
+            out += arr[j+i*width];
+        }
+        out += '"\n';
+    }
+
+    return out;
+}
+
+exports.xpm = xpm;
+}(typeof exports === 'undefined' ? this.savexpm = {} : exports));
---
+++ node/single.js
@@ -0,0 +1,21 @@
+var savexpm = require("../js/savexpm.js");
+var binaryheap = require("../js/binaryheap.js");
+var simulation = require("../js/simulation.js");
+
+var fs = require('fs');
+
+var alpha = 0.450;
+var filename = "cluster-pic.xpm";
+
+var board = new simulation.InfiniteBoard(1);
+var sim = new simulation.Simulation(board);
+
+sim.alpha = alpha;
+sim.mu = 1;
+sim.static_type = [simulation.S2Z, simulation.Z2R];
+sim.motion_type = [simulation.S2Z, simulation.Z2R];
+sim.addZombieSeed(0, 0, simulation.S2Z);
+
+while (sim.dostep()) {}
+
+fs.writeFileSync(filename, savexpm.xpm(sim.sites));
